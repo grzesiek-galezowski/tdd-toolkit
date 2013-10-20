1315ef622fc4a6d8c1ece3bb52f600f83d73dad2
grzesiek-galezowski <grzesiek.galezowski@gmail.com>
2013-10-20 21:11:25 +0200

    Added NUnit [Any] attribute extension

---
+++ TddToolkit/NUnitExtensions/AnyAttribute.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace TddEbook.TddToolkit.NUnitExtensions
+{
+  [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = false)]
+  public class AnyAttribute  : ParameterDataAttribute
+  {
+     public override IEnumerable GetData(ParameterInfo parameter)
+     {
+       return new[] { Any.Instance(parameter.ParameterType)};
+     }
+  }
+}
--- TddToolkitSpecification/AnySpecification.cs
+++ TddToolkitSpecification/AnySpecification.cs
@@ -5,6 +5,7 @@ using System.Reflection;
 using System.Text.RegularExpressions;
 using NUnit.Framework;
 using TddEbook.TddToolkit;
+using TddEbook.TddToolkit.NUnitExtensions;
 
 namespace TddToolkitSpecification
 {
