fe7904f8b5aae697839574e9ee5f38f60432f63f
grzesiek <spectral@op.pl>
2013-10-29 15:41:50 +0100

    Added class Empty (should the methods be rather properties?)

---
+++ TddToolkit/Empty.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TddEbook.TddToolkit
+{
+  public class Empty
+  {
+    public static List<T> List<T>()
+    {
+      return new List<T>();
+    }
+
+    public static ICollection<T> Collection<T>()
+    {
+      return List<T>();
+    }
+
+    public static T[] Array<T>()
+    {
+      return new T[0];
+    }
+
+    public static Dictionary<TKey, TValue> Dictionary<TKey, TValue>()
+    {
+      return new Dictionary<TKey, TValue>();
+    }
+
+    public static IEnumerable<T> Enumerable<T>()
+    {
+      return List<T>();
+    }
+
+    public static string String()
+    {
+      return string.Empty;
+    }
+   
+  }
+}
