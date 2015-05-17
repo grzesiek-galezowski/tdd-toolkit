3412470544702c710603861d33c0548be86a0ff1
grzesiek-galezowski <grzesiek.galezowski@gmail.com>
2015-05-17 12:05:42 +0200

    added UT

--- TddToolkitSpecification/XAssertSpecification.cs
+++ TddToolkitSpecification/XAssertSpecification.cs
@@ -44,6 +44,14 @@ namespace TddEbook.TddToolkitSpecification
     }
 
     [Test]
+    public void ShouldBeAbleToChooseInternalConstructorWhenThereisNoPublicOne()
+    {
+      Assert.DoesNotThrow(() => Any.Instance<FileNameWithoutExtension>());
+      Assert.DoesNotThrow(() => XAssert.IsValue<FileNameWithoutExtension>());
+    }
+
+
+    [Test]
     public void ShouldAllowSpecifyingConstructorArgumentsNotTakenIntoAccountDuringValueBehaviorCheck()
     {
       XAssert.IsValue<ProperValueTypeWithOneArgumentIdentity>(
@@ -378,6 +386,56 @@ namespace TddEbook.TddToolkitSpecification
       return !Equals(left, right);
     }
 
+  }
+
+  public class FileNameWithoutExtension : IEquatable<FileNameWithoutExtension>
+  {
+    public static FileNameWithoutExtension Value(string fileNameWithoutExtensionString)
+    {
+      return new FileNameWithoutExtension(fileNameWithoutExtensionString);
+    }
+
+    private readonly string _value;
+
+    internal FileNameWithoutExtension(string value)
+    {
+      _value = value;
+    }
+
+    public override bool Equals(object obj)
+    {
+      if (ReferenceEquals(null, obj)) return false;
+      if (ReferenceEquals(this, obj)) return true;
+      if (obj.GetType() != this.GetType()) return false;
+      return Equals((FileNameWithoutExtension)obj);
+    }
+
+    public bool Equals(FileNameWithoutExtension other)
+    {
+      if (ReferenceEquals(null, other)) return false;
+      if (ReferenceEquals(this, other)) return true;
+      return string.Equals(_value, other._value);
+    }
+
+    public override int GetHashCode()
+    {
+      return _value.GetHashCode();
+    }
+
+    public static bool operator ==(FileNameWithoutExtension left, FileNameWithoutExtension right)
+    {
+      return Equals(left, right);
+    }
+
+    public static bool operator !=(FileNameWithoutExtension left, FileNameWithoutExtension right)
+    {
+      return !Equals(left, right);
+    }
+
+    public override string ToString()
+    {
+      return _value;
+    }
   }
 }
 
