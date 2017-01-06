8991d4b57377348a2fc03c626fb1e4da1a97f620
Grzesiek Gałęzowski <grzesiek.galezowski@gmail.com>
2017-01-07 00:07:44 +0100

    Slight refactoring

--- TddToolkit/NSubstitute/XArg.cs
+++ TddToolkit/NSubstitute/XArg.cs
@@ -14,12 +14,12 @@ namespace TddEbook.TddToolkit.NSubstitute
   {
     public static T IsLike<T>(T expected)
     {
-      return IsEquivalentTo<T>(expected, new ObjectComparerEquivalencyAssertion());
+      return IsEquivalentTo(expected, new ObjectComparerEquivalencyAssertion());
     }
 
     public static T IsNotLike<T>(T expected)
     {
-      return IsEquivalentTo<T>(expected, new ObjectComparerNonEquivalencyAssertion());
+      return IsEquivalentTo(expected, new ObjectComparerNonEquivalencyAssertion());
     }
 
     public static T[] IsNot<T>(IEnumerable<T> unexpected)
@@ -33,33 +33,39 @@ namespace TddEbook.TddToolkit.NSubstitute
         .Should().NotBeEmpty("at least one condition should be specified");
 
       var equivalentArgumentMatcher = new EquivalentArgumentMatcher<T>(obj,
-        DetermineEquivalencyAssertion<T>(equivalencyAssertions));
-      SubstitutionContext.Current.EnqueueArgumentSpecification(new ArgumentSpecification(typeof(T),
-        equivalentArgumentMatcher));
+        DetermineEquivalencyAssertion(equivalencyAssertions));
+      EnqueueMatcher<T>(equivalentArgumentMatcher);
       return default(T);
     }
 
-    public static T Passing<T>(params Action<T>[] assertionActions)
+    public static T Passing<T>(params Action<T>[] assertions)
     {
-      assertionActions
+      assertions
         .Should().NotBeEmpty("at least one condition should be specified");
 
-      var lambdaMatcher = new LambdaArgumentMatcher<T>(assertionActions);
-      SubstitutionContext.Current.EnqueueArgumentSpecification(
-        new ArgumentSpecification(typeof(T), lambdaMatcher));
+      var lambdaMatcher = new LambdaArgumentMatcher<T>(assertions);
+      EnqueueMatcher<T>(lambdaMatcher);
       return default(T);
     }
 
-    private static EquivalencyAssertion DetermineEquivalencyAssertion<T>(EquivalencyAssertion[] equivalencyAssertions)
+
+
+    private static void EnqueueMatcher<T>(IArgumentMatcher lambdaMatcher)
+    {
+      SubstitutionContext.Current.EnqueueArgumentSpecification(
+        new ArgumentSpecification(typeof(T), 
+          lambdaMatcher));
+    }
+    public static T IsEquivalentTo<T>(T obj)
+    {
+      return IsEquivalentTo(obj, FluentAssertionsEquivalencyAssertion<T>.Default());
+    }
+
+    private static EquivalencyAssertion DetermineEquivalencyAssertion(EquivalencyAssertion[] equivalencyAssertions)
     {
       return equivalencyAssertions.Length == 1 ? 
         equivalencyAssertions.First()
         : new CompoundEquivalencyAssertion(equivalencyAssertions);
-    }
-
-    public static T IsEquivalentTo<T>(T obj)
-    {
-      return IsEquivalentTo(obj, FluentAssertionsEquivalencyAssertion<T>.Default());
     }
   }
 }
