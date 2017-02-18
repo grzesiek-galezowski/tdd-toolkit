f7e9cc36fa962b6f327b7ee3af651a1f4a2acbaf
Grzesiek Gałęzowski <grzesiek.galezowski@gmail.com>
2017-02-18 22:44:38 +0100

    changed names

--- TddToolkit/Any.Numbers.cs
+++ TddToolkit/Any.Numbers.cs
@@ -12,120 +12,149 @@ namespace TddEbook.TddToolkit
   {
     public static int Integer()
     {
+      return _any.Integer();
     }
 
     public static double Double()
     {
+      return _any.Double();
     }
 
     public static double DoubleOtherThan(params double[] others)
     {
+      return _any.DoubleOtherThan(others);
     }
 
     public static long LongInteger()
     {
+      return _any.LongInteger();
     }
 
     public static long LongIntegerOtherThan(params long[] others)
     {
+      return _any.LongIntegerOtherThan(others);
     }
 
     public static ulong UnsignedLongInteger()
     {
+      return _any.UnsignedLongInteger();
     }
 
     public static ulong UnsignedLongIntegerOtherThan(params ulong[] others)
     {
+      return _any.UnsignedLongIntegerOtherThan(others);
     }
 
     public static int IntegerOtherThan(params int[] others)
     {
+      return _any.IntegerOtherThan(others);
     }
 
     public static byte Byte()
     {
+      return _any.Byte();
     }
 
     public static byte ByteOtherThan(params byte[] others)
     {
+      return _any.ByteOtherThan(others);
     }
 
     public static decimal Decimal()
     {
+      return _any.Decimal();
     }
 
     public static decimal DecimalOtherThan(params decimal[] others)
     {
+      return _any.DecimalOtherThan(others);
     }
 
     public static uint UnsignedInt()
     {
+      return _any.UnsignedInt();
     }
 
     public static uint UnsignedIntOtherThan(params uint[] others)
     {
+      return _any.UnsignedIntOtherThan(others);
     }
 
     public static ushort UnsignedShort()
     {
+      return _any.UnsignedShort();
     }
 
     public static ushort UnsignedShortOtherThan(params ushort[] others)
     {
+      return _any.UnsignedShortOtherThan(others);
     }
 
     public static short ShortInteger()
     {
+      return _any.ShortInteger();
     }
 
     public static short ShortIntegerOtherThan(params short[] others)
     {
+      return _any.ShortIntegerOtherThan(others);
     }
 
     public static byte Digit()
     {
+      return _any.Digit();
     }
 
     public static int IntegerFromSequence(int startingValue = 0, int step = 1)
     {
+      return _any.IntegerFromSequence(startingValue, step);
     }
 
     public static byte Octet()
     {
+      return _any.Octet();
     }
 
     public static int IntegerDivisibleBy(int quotient)
     {
+      return _any.IntegerDivisibleBy(quotient);
     }
 
     public static int IntegerNotDivisibleBy(int quotient)
     {
+      return _any.IntegerNotDivisibleBy(quotient);
     }
 
     private static void AssertQuotientMakesSense(int quotient)
     {
+      _any.AssertQuotientMakesSense(quotient);
     }
 
 
     public static int IntegerWithExactDigitsCount(int digitsCount)
     {
+      return _any.IntegerWithExactDigitsCount(digitsCount);
     }
 
     public static long LongIntegerWithExactDigitsCount(int digitsCount)
     {
+      return _any.LongIntegerWithExactDigitsCount(digitsCount);
     }
 
     public static uint UnsignedIntegerWithExactDigitsCount(int digitsCount)
     {
+      return _any.UnsignedIntegerWithExactDigitsCount(digitsCount);
     }
 
     public static ulong UnsignedLongIntegerWithExactDigitsCount(int digitsCount)
     {
+      return _any.UnsignedLongIntegerWithExactDigitsCount(digitsCount);
     }
 
 
     public static byte PositiveDigit()
     {
+      return _any.PositiveDigit();
     }
 
   }
--- TddToolkit/Any.Strings.cs
+++ TddToolkit/Any.Strings.cs
@@ -11,90 +11,113 @@ namespace TddEbook.TddToolkit
   {
     public static string String()
     {
+      return _any.String();
     }
 
     public static string String(string seed)
     {
+      return _any.String(seed);
     }
 
     public static string LowerCaseString()
     {
+      return _any.LowerCaseString();
     }
 
     public static string UpperCaseString()
     {
+      return _any.UpperCaseString();
     }
 
     public static string LowerCaseAlphaString()
     {
+      return _any.LowerCaseAlphaString();
     }
 
     public static string UpperCaseAlphaString()
     {
+      return _any.UpperCaseAlphaString();
     }
 
 
     public static string StringMatching(string pattern)
     {
+      return _any.StringMatching(pattern);
     }
 
     public static string StringOfLength(int charactersCount)
     {
+      return _any.StringOfLength(charactersCount);
     }
 
     public static string StringOtherThan(params string[] alreadyUsedStrings)
     {
+      return _any.StringOtherThan(alreadyUsedStrings);
     }
 
     public static string StringNotContaining<T>(params T[] excludedObjects)
     {
+      return _any.StringNotContaining(excludedObjects);
     }
 
     public static string StringNotContaining(params string[] excludedSubstrings)
     {
+      return _any.StringNotContaining(excludedSubstrings);
     }
 
     public static string StringContaining<T>(T obj)
     {
+      return _any.StringContaining(obj);
     }
 
     public static string StringContaining(string str)
     {
+      return _any.StringContaining(str);
     }
 
     public static string AlphaString()
     {
+      return _any.AlphaString();
     }
 
     public static string AlphaString(int maxLength)
     {
+      return _any.AlphaString(maxLength);
     }
 
     public static string Identifier()
     {
+      return _any.Identifier();
     }
 
     public static char AlphaChar()
     {
+      return _any.AlphaChar();
     }
 
     public static char DigitChar()
     {
+      return _any.DigitChar();
     }
 
     public static char Char()
     {
+      return _any.Char();
     }
 
+    public static string NumericString(int digitsCount = AllGenerator.Many)
     {
+      return _any.NumericString(digitsCount);
     }
 
     public static char LowerCaseAlphaChar()
     {
+      return _any.LowerCaseAlphaChar();
     }
 
     public static char UpperCaseAlphaChar()
     {
+      return _any.UpperCaseAlphaChar();
     }
   }
 }
